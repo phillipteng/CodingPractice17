06e4e48d3c4e8ee49316ce7a627e0ce3d4edb757
phillip <phillipt@bu.edu>
2020-10-23 01:30:51 +0800

    Added codeforces folder

--- main.cpp
+++ main.cpp
@@ -3,61 +3,97 @@
 //
 #include <iostream>
 #include <vector>
+#include <utility>
 
-typedef long long ll;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a; i < b; ++i)
+
+
 
 using namespace std;
 
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
 
-int main(){
-    int d, sumTime;
-
-    cin >> d >> sumTime;
-    int totalMax = 0;
-    int totalMin = 0;
-    //calculate the max and keep track the number of days that i can study less.
-    //if the answer is between the possible numbers then, we take the max and keep subtracting max from the first day
-    //until we get to the end
-
-    vector<int> runningMin;
-    vector<int> runningMax;
-
-    while(d--){
-        int minDay, maxDay;
-        cin >> minDay >> maxDay;
-        runningMin.push_back(minDay);
-        runningMax.push_back(maxDay);
-        totalMin+=minDay;
-        totalMax+=maxDay;
+class Solution {
+public:
+    int f(int x){
+        for (int power = 5; power <= x; power*=5){
+            ans += x/ power;
+        }
+        return ans;
     }
+    int preimageSizeFZF(int K) {
+        int low = 0, high = INF;
+        while(low <= high){
+            int x = (low + high) / 2;
 
-    vector<int> minSums;
-    minSums.push_back(0);
-    int runningSum = 0;
-    for (int i = runningMin.size() - 1; i >= 0; --i){
-        runningSum += runningMin[i];
-        minSums.push_back(runningSum);
-    }
-
-    if (sumTime >= totalMin && sumTime <= totalMax){
-        // begin with the max, then subtract until i get to the sum then print it
-        cout << "YES" << endl;
-        int maxSum = 0;
-        for (int i = 0; i < runningMax.size(); ++i){
-            maxSum += runningMax[i];
-            if (maxSum + minSums[i] <= sumTime){
-                cout << runningMax[i] << " ";
+            if (f(x) >K){
+                high = x - 1;
             }
-            else {
-                int diff = sumTime - maxSum;
-                int answer = runningMax[i] + diff;
-                cout << answer << " ";
+            else if (f(x) < K ){
+
             }
         }
+
+        // numb
+        //just come up with the exp of 5
+
     }
-    else{
-        cout << "NO" << endl;
-    }
+};
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    //read in inputs
+    int testCases;
+    cin >> testCases;
+
+    while(testCases--){
+        int numOfDistricts;
+        cin >> numOfDistricts;
+        vector<int> gangList;
+        while(numOfDistricts--){
+            int gangNum;
+            cin >> gangNum;
+            gangList.PB(gangNum);
+        }
+
+        //check if it is possible, if all the numbers are the same then it it not possible
+        int firstNum = gangList[0];
+        bool answer = false;
+        REP (i, 1, gangList.size()){
+            if (gangList[i] != firstNum){
+                //possible
+                answer = true;
+                break;
+            }
+        }
+
+        if (!answer){
+            cout << "NO" << endl;
+        }
+        else {
+            cout << "YES" << endl;
+        }
+
+        vector<vector<int>> adjList;
+        vector<int> emptyList;
+        for(auto i: gangList){
+            adjList.PB(emptyList);
+        }
+
+        //keep adding districts to the existing node until u have added all of the districts
+        while(!gangList.empty()){
+            // n^2 solution brute force
+        }
+
+    };
 
     return 0;
 }
